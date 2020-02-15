ccdb7e024ab0e84c08c135bdd6550a0bcab6d338
JP <339y4j@gmail.com>
2020-02-15 21:24:37 +0800

    Removing card header and title.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
-  { path: "", redirectTo: "menu", pathMatch: "full" },
+  { path: "", redirectTo: "/menu/main", pathMatch: "full" },
   {
     path: "menu",
     loadChildren: "./pages/menu/menu.module#MenuPageModule"
